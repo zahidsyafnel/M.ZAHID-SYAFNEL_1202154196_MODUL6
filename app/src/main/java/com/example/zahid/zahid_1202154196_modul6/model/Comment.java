a0daecba0a1e3800756007ee7bee32d74fca50ea
zahidsyafnel <zahidsyafnel@gmail.com>
2018-04-01 22:09:08 +0700

    push sedikit bosku modul terakhir

---
+++ app/src/main/java/com/example/zahid/zahid_1202154196_modul6/model/Comment.java
@@ -0,0 +1,32 @@
+package com.example.zahid.zahid_1202154196_modul6.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Comment {
+    public String uid;
+
+    public String author;
+
+    public String text;
+
+
+
+    public Comment() {
+
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+
+    }
+
+
+
+    public Comment(String uid, String author, String text) {
+
+        this.uid = uid;
+
+        this.author = author;
+
+        this.text = text;
+
+    }
+}
