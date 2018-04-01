bef2290d1e407264572db98fe2dbd49e9f4b2b98
zahidsyafnel <zahidsyafnel@gmail.com>
2018-04-01 23:12:30 +0700

    push kedua kali

--- app/src/androidTest/java/com/example/zahid/zahid_1202154196_modul6/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/zahid/zahid_1202154196_modul6/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.galuhwisnupambayun.galuh_1202150076_modul6", appContext.getPackageName());
+        assertEquals("com.example.zahid.zahid_1202154196_modul6", appContext.getPackageName());
     }
 }
